970a79e3000de2baaeeffc2e1a802c7d0d572b61
dlgusgh0812 <tkfkd08gkrl12>
2021-07-08 14:56:19 +0900

    header 반응형 작업

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import { Link , animateScroll as scroll} from 'react-scroll'
-
 import '../assets/css/header.css';
 
 class Header extends Component {
@@ -8,14 +7,38 @@ class Header extends Component {
         return (
           <header>
             <nav class="menu">
-              <ul>
+              <ul class="big-size-menu">
                 <li><a onClick={() => scroll.scrollToTop()}>About Me</a></li>
                 <li><Link activeClass="active" to="skills_Container" spy={true} smooth={true} duration={600} >Skills</Link></li>
                 <li><Link activeClass="active" to="work_Container" spy={true} smooth={true} duration={600} >Work Experiences</Link></li>
                 <li><Link activeClass="active" to="project_Container" spy={true} smooth={true} duration={600} >Project</Link></li>
-                <li><Link activeClass="active" to="blog_container" spy={true} smooth={true} duration={600} >Blog</Link></li>
-                <li><Link activeClass="active" to="contactme_container" spy={true} smooth={true} duration={600} >Contact Me</Link></li>
               </ul>
+              <ul class="small-size-menu">
+                <li>
+                  <a onClick={() => scroll.scrollToTop()}>
+                    <i class="fas fa-id-card"></i>
+                  </a>
+                </li>
+                <li>
+                  <Link activeClass="active" to="skills_Container"
+                  spy={true} smooth={true} duration={600}>
+                    <i class="fas fa-code"></i>
+                  </Link>
+                </li>
+                <li>
+                  <Link activeClass="active" to="work_Container"
+                  spy={true} smooth={true} duration={600}>
+                    <i class="fas fa-laptop-code"></i>
+                  </Link>
+                </li>
+                <li>
+                  <Link activeClass="active" to="project_Container"
+                  spy={true} smooth={true} duration={600}>
+                    <i class="fas fa-chalkboard-teacher"></i>
+                  </Link>
+                </li>
+              </ul>
+
             </nav>
 
           </header>

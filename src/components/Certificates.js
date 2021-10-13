0c8d8727ed04cb4ce4612c221dd08c5417196e74
leehyeonho <dlgusgh0812@gmail.com>
2021-10-13 18:17:00 +0900

    certificates를 추가한다.

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -1,54 +1,50 @@
 import React, {Component} from 'react';
 import '../assets/css/Certificates.css';
+import java_spring_1 from '../assets/images/Certificates/java-spring-1.jpg';
+import java_spring_2 from '../assets/images/Certificates/java-spring-2.jpg';
+import java_spring_3 from '../assets/images/Certificates/java-spring-3.jpg';
+import css_layout from '../assets/images/Certificates/css-layout.jpg';
+import python from '../assets/images/Certificates/python.jpg';
+import vanillaJS from '../assets/images/Certificates/vanillaJS.jpg';
 
 class Certificates extends Component {
     render() {
         return (
           <div className="container Certificates_Container" id="Certificates_Container" name="skills_Container">
-            <h1 className="Certificates">Skills</h1>
+            <h1 className="Certificates">Certificates</h1>
             <hr className="hr_Certificates"></hr>
-            <ul>
-              <li><i className="fa fa-html5 fa-3x" ></i><p>HTML5</p></li>
-              <li><i className="fab fa-css3-alt fa-3x" ></i><p>CSS3</p></li>
-              <li><i className="fab fa-js fa-3x" ></i><p>JS</p></li>
-              <li><i className="fab fa-react fa-3x" ></i><p>REACTJS</p></li>
-              <li><i className="fab fa-node fa-3x" ></i><p>NODEJS</p></li>
-              <li><i className="fab fa-java fa-3x" ></i><p>JAVA</p></li>
-              <li>
-                <div className="img_crop">
-                  <img src={rails} alt=""></img><p>RUBY ON RAILS</p>
-                </div>
-              </li>
-              <li>
-                <div className="img_crop">
-                  <img src={jquery_icon} alt=""></img><p>jquery</p>
-                </div>
-              </li>
-              <li>
-                <div className="img_crop">
-                  <img src={python_icon} alt=""></img><p>python</p>
-                </div>
-              </li>
-              <li>
-                <div className="img_crop">
-                  <img src={mysql_icon} alt=""></img><p>python</p>
-                </div>
-              </li>
-              <li></li>
-              <li></li>
-            </ul>
-            <div className="nomad_container">
-              <div className="ring">
-                <div className="img"></div>
-                <div className="img"></div>
-                <div className="img"></div>
-                <div className="img"></div>
-                <div className="img"></div>
-                <div className="img"></div>
-                <div className="img"></div>
-                <div className="img"></div>
-                <div className="img"></div>
-                <div className="img"></div>
+            <div className="cert_inner_container">
+              <ul className="cert_list">
+                <li className="cert_card">
+                  <div className="blur">
+                  디지털컨버전스 자바기반 응용SW엔지니어링 양성훈련과정
+                  </div>
+                  <img src={java_spring_1} alt=""></img>
+                </li>
+                <li className="cert_card">
+                  <div className="blur">
+                    CSS Layout(flex, grid) / Nomad Coders
+                  </div>
+                  <img src={css_layout} alt=""></img>
+                </li>
+                <li className="cert_card">
+                  <div className="blur">
+                    파이썬(python, flask, beautifulSoup) / Nomad Coders
+                  </div>
+                  <img src={python} alt=""></img>
+                </li>
+                <li className="cert_card">
+                  <div className="blur">
+                    바닐라JS(vanillaJS) / Nomad Coders
+                  </div>
+                  <img src={vanillaJS} alt=""></img>
+                </li>
+              </ul>
+              
+            </div>
+            <div className="cert_pagenation">
+              <div className="cert_pages">
+
               </div>
             </div>
           </div>
